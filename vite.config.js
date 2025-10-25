e5a0e9387cd2e08c52f0615b8a298d542fa86b7e
adityasairam1 <adityapsairam@gmail.com>
2025-10-25 11:26:11 -0400

    updates on vercel

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    copyPublicDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          framer: ['framer-motion'],
-          icons: ['react-icons']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  preview: {
-    port: 4173,
-    open: true
-  }
+  base: '/',
+  publicDir: 'public'
 })
